afc11ab2becc0dc3933df4d2e1ff05c215d5b4ed
beatrizzie <laillabbf0507@gmail.com>
2026-05-20 00:12:02 -0300

    Criação do main.py e integração do sistema

--- login.py
+++ login.py
@@ -2,64 +2,71 @@ import tkinter as tk
 from tkinter import messagebox 
 from database import init_db, add_user, validate_user
 
-init_db()
+from busca import abrir_busca
 
-root = tk.Tk()
-root.title("Login Form")
-root.geometry("350x300")
-root.config(bg="#f0f0f0")
-root.resizable(False, False)
+def abrir_login():
+    print("Abrindo tela de login...")
+    init_db()
 
-username_var = tk.StringVar()
-password_var = tk.StringVar()
-show_password = tk.BooleanVar(value=False)
-remember_me = tk.BooleanVar()
+    root = tk.Tk()
+    print
+    root.title("Login Form")
+    root.geometry("350x300")
+    root.config(bg="#f0f0f0")
+    root.resizable(False, False)
 
-def login():
-    user = username_var.get()
-    pwd = password_var.get()
-    if validate_user(user, pwd):
-        messagebox.showinfo("Sucesso", "Login bem-sucedido!")
-    else:
-        messagebox.showerror("Erro", "Usuário ou senha incorretos.")
+    username_var = tk.StringVar()
+    password_var = tk.StringVar()
+    show_password = tk.BooleanVar(value=False)
+    remember_me = tk.BooleanVar()
 
-def register():
-    user = username_var.get()
-    pwd = password_var.get()
-    if add_user(user, pwd):
-        messagebox.showinfo("Sucesso", "Usuário registrado com sucesso!")
-    else:
-        messagebox.showerror("Erro", "Usuário já existe.")
+    def login():
+        user = username_var.get()
+        pwd = password_var.get()
+        if validate_user(user, pwd):
+            messagebox.showinfo("Sucesso", "Login bem-sucedido!")
+            root.destroy()
+            abrir_busca()
+        else:
+            messagebox.showerror("Erro", "Usuário ou senha incorretos.")
 
-def toggle_password():
-    if show_password.get():
-        password_entry.config(show="")
-    else:
-        password_entry.config(show="*")
+    def register():
+        user = username_var.get()
+        pwd = password_var.get()
+        if add_user(user, pwd):
+            messagebox.showinfo("Sucesso", "Usuário registrado com sucesso!")
+        else:
+            messagebox.showerror("Erro", "Usuário já existe.")
 
-#titulo
-tk.Label(root, text="Login", font=("Arial", 19, "bold"), bg="#f0f0f0").pack(pady=10)
+    def toggle_password():
+        if show_password.get():
+            password_entry.config(show="")
+        else:
+            password_entry.config(show="*")
 
-#usuario
-tk.Label(root, text="Usuário:", bg="#f0f0f0").pack(anchor="w", padx=30)
-tk.Entry(root, textvariable=username_var, width=30).pack(pady=2)
+    #titulo
+    tk.Label(root, text="Login", font=("Arial", 19, "bold"), bg="#f0f0f0").pack(pady=10)
 
-#senha
-tk.Label(root, text="Senha:", bg="#f0f0f0").pack(anchor="w", padx=30)
-password_entry = tk.Entry(root, textvariable=password_var, width=30, show="*")
-password_entry.pack(pady=2)
+    #usuario
+    tk.Label(root, text="Usuário:", bg="#f0f0f0").pack(anchor="w", padx=30)
+    tk.Entry(root, textvariable=username_var, width=30).pack(pady=2)
 
-#visualizar senha
-toggle_btn = tk.Checkbutton(root, text="Mostrar senha", variable=show_password, bg="#f0f0f0", command=toggle_password)
-toggle_btn.pack(anchor="w", padx=30,pady=5)
+    #senha
+    tk.Label(root, text="Senha:", bg="#f0f0f0").pack(anchor="w", padx=30)
+    password_entry = tk.Entry(root, textvariable=password_var, width=30, show="*")
+    password_entry.pack(pady=2)
 
-#lembrarsenha
-remember_cb = tk.Checkbutton(root, text="Lembrar senha", variable=remember_me, bg="#f0f0f0")
-remember_cb.pack(anchor="w", padx=30,pady=5)
+    #visualizar senha
+    toggle_btn = tk.Checkbutton(root, text="Mostrar senha", variable=show_password, bg="#f0f0f0", command=toggle_password)
+    toggle_btn.pack(anchor="w", padx=30,pady=5)
 
-#botoes de login ou registro
-tk.Button(root, text="Login", command=login, width=25, bg="#9d88e9", fg="white").pack(pady=8)
-tk.Button(root, text="Registro", command=register, width=25, bg="#725dbb", fg="white").pack()
+    #lembrarsenha
+    remember_cb = tk.Checkbutton(root, text="Lembrar senha", variable=remember_me, bg="#f0f0f0")
+    remember_cb.pack(anchor="w", padx=30,pady=5)
+
+    #botoes de login ou registro
+    tk.Button(root, text="Login", command=login, width=25, bg="#9d88e9", fg="white").pack(pady=8)
+    tk.Button(root, text="Registro", command=register, width=25, bg="#725dbb", fg="white").pack()
 
 
-root.mainloop()
+    root.mainloop()
---
+++ main.py
@@ -0,0 +1,3 @@
+print("Iniciando aplicação...")
+from login import abrir_login
+abrir_login()
